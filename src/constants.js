e8214f879c969ae02c11e70ed061e8b0a91fe2ae
Douae sb <douae.sb411@gmail.com>
2024-12-28 22:55:07 +0100

    portfolio

---
+++ src/constants.js
@@ -0,0 +1,71 @@
+export const techStack = [
+  { name: "Java", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg" },
+  { name: "JavaScript", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
+  { name: "C Language", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-plain.svg" },
+  { name: "CSS", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg" },
+  { name: "HTML", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" },
+  { name: "PHP", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg" },
+  { name: "TypeScript", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" },
+  
+  { name: "React", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" },
+  { name: "React Native", link: "https://pagepro.co/blog/wp-content/uploads/2020/03/react-native-logo-884x1024.png" },
+  { name: "Angular", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angular/angular-original.svg" },
+  
+  { name: "Tailwind CSS", link: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" },
+  { name: "Bootstrap", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg" },
+  
+  { name: "MySQL", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg" },
+  { name: "MongoDB", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain.svg" },
+  
+  { name: "Git", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg" },
+  { name: "Docker", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain.svg" },
+  { name: "VS Code", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" },
+  
+  { name: "GitHub", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" },
+  { name: "Postman", link: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" },
+  
+  { name: "Jenkins", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jenkins/jenkins-original.svg" },
+  { name: "Trello", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/trello/trello-plain.svg" },
+  { name: "Jira", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jira/jira-original-wordmark.svg" },
+  
+  { name: "GitHub Pages", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" },
+  { name: "Spring Boot", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original-wordmark.svg" },
+  { name: "Laravel", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/laravel/laravel-plain.svg" },
+  { name: "API", link: "https://img.icons8.com/ios/50/000000/api.png" },
+  { name: "NodeJS", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" }
+
+
+];
+
+
+export const contactLinks = [{ name: 'Stack Overflow', url: 'https://img.icons8.com/external-tal-revivo-color-tal-revivo/40/000000/external-stack-overflow-is-a-question-and-answer-site-for-professional-logo-color-tal-revivo.png', link: 'https://stackoverflow.com/users/23039888/douaesb' },
+{ name: 'Github', url: 'https://img.icons8.com/doodle/40/000000/github--v1.png', link :'https://github.com/Douaesb'},
+{ name: 'LinkedIn', url: 'https://img.icons8.com/doodle/40/000000/linkedin--v2.png', link: 'https://www.linkedin.com/in/douae-sebti-6a177b23b/'}
+]
+
+
+
+export const serviceData = [
+
+
+  {
+    name: 'Web Application Development',
+    desc: 'Web application development involves the creation of interactive and dynamic websites that combine web design and functionality. It includes skills such as web graphic design, user interface (UI) design, authoring with standardized code, user experience (UX) design, and search engine optimization (SEO). This ensures robust and scalable web solutions.',
+    img: "https://img.icons8.com/external-kiranshastry-lineal-color-kiranshastry/64/000000/external-web-development-coding-kiranshastry-lineal-color-kiranshastry.png"
+  },
+  
+  
+  {
+    name: 'Front-End Development',
+    desc: `Front-end development involves crafting the user interface and experience of web applications using technologies like HTML, CSS, and JavaScript. It emphasizes responsive design, interactivity, and performance optimization to deliver seamless experiences across devices and browsers.`,
+    img: "https://img.icons8.com/external-kiranshastry-lineal-color-kiranshastry/64/000000/external-web-design-coding-kiranshastry-lineal-color-kiranshastry.png"
+  },
+  {
+    name: 'Backend Development',
+    desc: `Backend development focuses on server-side programming to ensure the functionality, scalability, and security of applications. It involves creating APIs, managing databases, and handling server logic to process and serve client requests effectively. This is the backbone of modern applications.`,
+    img: "https://img.icons8.com/external-flaticons-flat-flat-icons/64/000000/external-backend-no-code-flaticons-flat-flat-icons.png"
+  }
+
+]
+
+
---
+++ src/views/About.jsx
@@ -0,0 +1,85 @@
+import React, { useContext } from "react";
+import { techStack } from "../constants";
+import { ThemeContext } from "../themeProvider";
+import { motion } from "framer-motion";
+
+const About = () => {
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+  return (
+    <div id="about" className={darkMode === true ? "bg-white" : "bg-gray-900"}>
+      <div className="max-w-7xl mx-auto x-4 sm:px-6 lg:px-8 px-4 md:mt-0 pt-24 pb-12">
+        <h2
+          className={
+            darkMode
+              ? "text-5xl font-bold px-4 md:px-0 text-center"
+              : "text-5xl font-bold px-4 md:px-0 text-center text-white"
+          }
+        >
+          About Me
+        </h2>
+        <div>
+          <motion.div>
+            <h4 className="mt-12 text-3xl font-semibold text-blue-500">
+              A bit about me
+            </h4>
+            <p
+              className={
+                darkMode
+                  ? "mt-4 text-xl text-justify text-gray-500"
+                  : "mt-4 text-xl text-justify text-white"
+              }
+            >
+             As a dedicated Full Stack Developer, I specialize in creating seamless, efficient, and innovative web, desktop, mobile applications. My expertise spans both front-end and back-end development, utilizing a diverse skill set that includes HTML, CSS, Tailwind, Bootstrap,JavaScript, React native, React js, Java, Spring boot, php, laravel, MySQL, PostgreSQL, API management, Docker and Postman.
+
+I thrive in dynamic environments, tackling complex problems and delivering high-quality solutions. Whether collaborating with cross-functional teams or leading independent projects, I am committed to continuous learning and leveraging the latest technologies to drive impactful results.
+
+My passion for coding is matched by my enthusiasm for understanding user needs and translating them into intuitive digital experiences. Let's connect and explore how I can contribute to your next big project.
+            </p>
+          </motion.div>
+          <motion.div
+          >
+            <h4 className="mt-12 text-3xl font-semibold text-blue-500">
+              Technologies and Tools
+            </h4>
+            <p
+              className={
+                darkMode
+                  ? "mt-4 text-xl text-justify text-gray-500"
+                  : "mt-4 text-xl text-justify text-white"
+              }
+            >
+              Using a combination of cutting-edge technologies and reliable
+              open-source software I build user-focused, performant apps and
+              websites for smartphones, tablets, and desktops.
+            </p>
+          </motion.div>
+          <motion.div className="flex flex-wrap mt-8 flex flex-wrap justify-between ">
+            {techStack.map((el, index) => (
+              <motion.div
+                initial="hidden"
+                whileInView={"visible"}
+                variants={{
+                  visible: {
+                    y: 0,
+                    opacity: 1,
+                    transition: {
+                      type: "spring",
+                    },
+                  },
+                  hidden: { opacity: 1, y: 80 },
+                }}
+                className="py-2 px-4 bg-gray-50 md:m-4 mx-2 mt-6 rounded-lg flex items-center hover:scale-125 cursor-pointer md:w-48 w-40"
+              >
+                <img alt="" src={el.link} className="w-12" />
+                <h4 className="text-md ml-4">{el.name}</h4>
+              </motion.div>
+            ))}
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
